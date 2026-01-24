88cdd4cfe74da990070e38e4301e5176e1e0e10f
syboosyboo <john782@ajou.ac.kr>
2026-01-24 16:53:37 +0900

    #19 feat: Querydsl을 이용한 공지사항 검색 레포지토리 구현

--- src/main/java/syboo/notice/notice/repository/NoticeRepository.java
+++ src/main/java/syboo/notice/notice/repository/NoticeRepository.java
@@ -3,5 +3,5 @@ package syboo.notice.notice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import syboo.notice.notice.domain.Notice;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeQueryRepository {
 }
